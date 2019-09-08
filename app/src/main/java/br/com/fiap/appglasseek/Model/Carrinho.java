d5933d4ec9959c85dac3bef1ecef1d1bfe540bb1
Guilherme Bigois <guilhermebigois@outlook.com>
2019-09-08 03:19:52 -0300

    Carrinho class created

---
+++ app/src/main/java/br/com/fiap/appglasseek/Model/Carrinho.java
@@ -0,0 +1,22 @@
+package br.com.fiap.appglasseek.Model;
+
+import java.util.List;
+
+public class Carrinho {
+    private List<Oculos> oculos;
+
+    public Carrinho(List<Oculos> oculos) {
+        this.oculos = oculos;
+    }
+
+    public Carrinho() {
+    }
+
+    public List<Oculos> getOculos() {
+        return oculos;
+    }
+
+    public void setOculos(List<Oculos> oculos) {
+        this.oculos = oculos;
+    }
+}
