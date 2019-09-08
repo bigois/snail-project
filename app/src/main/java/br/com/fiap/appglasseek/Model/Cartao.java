e3864a3a1b6810986e3b8413e4eb8ef5e956907d
Guilherme Bigois <guilhermebigois@outlook.com>
2019-09-08 03:13:12 -0300

    Cartao class created

---
+++ app/src/main/java/br/com/fiap/appglasseek/Model/Cartao.java
@@ -0,0 +1,50 @@
+package br.com.fiap.appglasseek.Model;
+
+public class Cartao {
+    private String numero;
+    private String nome;
+    private String validade;
+    private String tipo;
+
+    public Cartao(String numero, String nome, String validade, String tipo) {
+        this.numero = numero;
+        this.nome = nome;
+        this.validade = validade;
+        this.tipo = tipo;
+    }
+
+    public Cartao() {
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getValidade() {
+        return validade;
+    }
+
+    public void setValidade(String validade) {
+        this.validade = validade;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
