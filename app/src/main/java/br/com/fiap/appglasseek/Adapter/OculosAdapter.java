da9da7bf49520e4638a452180575b980488b3469
Guilherme Bigois <guilhermebigois@outlook.com>
2019-09-08 04:34:56 -0300

    Code rearranged

--- app/src/main/java/br/com/fiap/appglasseek/Adapter/OculosAdapter.java
+++ app/src/main/java/br/com/fiap/appglasseek/Adapter/OculosAdapter.java
@@ -6,7 +6,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import java.text.DecimalFormat;
 import java.util.List;
 
 import br.com.fiap.appglasseek.Holder.OculosHolder;
--- app/src/main/java/br/com/fiap/appglasseek/Holder/OculosHolder.java
+++ app/src/main/java/br/com/fiap/appglasseek/Holder/OculosHolder.java
@@ -1,6 +1,5 @@
 package br.com.fiap.appglasseek.Holder;
 
-import android.media.Image;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
