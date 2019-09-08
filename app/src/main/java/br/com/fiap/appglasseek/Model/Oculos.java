e89bc9187ecc04670fff01fad80299873e9d528b
Guilherme Bigois <guilhermebigois@outlook.com>
2019-09-08 00:10:44 -0300

    Oculos Activity Created

--- app/src/main/java/br/com/fiap/appglasseek/Model/Oculos.java
+++ app/src/main/java/br/com/fiap/appglasseek/Model/Oculos.java
@@ -3,46 +3,41 @@ package br.com.fiap.appglasseek.Model;
 import android.media.Image;
 
 public class Oculos {
-    private String id;
-    private String nome;
-    private String categoria;
+    private String cod;
     private String marca;
+    private String modelo;
+    private String tipo;
+    private String genero;
     private String cor;
-    private Double preco;
-    private Image imagemOculos;
+    private Double comprimento;
+    private Double largura;
+    private Double altura;
+    private String material;
+    private Image imagem;
 
-    public Oculos(String id, String nome, String categoria, String marca, String cor, Double preco, Image imagemOculos) {
-        this.id = id;
-        this.nome = nome;
-        this.categoria = categoria;
+    public Oculos(String cod, String marca, String modelo, String tipo, String genero, String cor, Double comprimento, Double largura, Double altura, String material, Image imagem) {
+        this.cod = cod;
         this.marca = marca;
+        this.modelo = modelo;
+        this.tipo = tipo;
+        this.genero = genero;
         this.cor = cor;
-        this.preco = preco;
-        this.imagemOculos = imagemOculos;
+        this.comprimento = comprimento;
+        this.largura = largura;
+        this.altura = altura;
+        this.material = material;
+        this.imagem = imagem;
     }
 
-    public String getId() {
-        return id;
+    public Oculos() {
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public String getCod() {
+        return cod;
     }
 
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCategoria() {
-        return categoria;
-    }
-
-    public void setCategoria(String categoria) {
-        this.categoria = categoria;
+    public void setCod(String cod) {
+        this.cod = cod;
     }
 
     public String getMarca() {
@@ -53,6 +48,30 @@ public class Oculos {
         this.marca = marca;
     }
 
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
     public String getCor() {
         return cor;
     }
@@ -61,19 +80,35 @@ public class Oculos {
         this.cor = cor;
     }
 
-    public Double getPreco() {
-        return preco;
+    public Double getComprimento() {
+        return comprimento;
     }
 
-    public void setPreco(Double preco) {
-        this.preco = preco;
+    public void setComprimento(Double comprimento) {
+        this.comprimento = comprimento;
     }
 
-    public Image getImagemOculos() {
-        return imagemOculos;
+    public Double getLargura() {
+        return largura;
     }
 
-    public void setImagemOculos(Image imagemOculos) {
-        this.imagemOculos = imagemOculos;
+    public void setLargura(Double largura) {
+        this.largura = largura;
+    }
+
+    public Double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(Double altura) {
+        this.altura = altura;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
     }
 }
---
+++ app/src/main/java/br/com/fiap/appglasseek/OculosActivity.java
@@ -0,0 +1,13 @@
+package br.com.fiap.appglasseek;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OculosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_oculos);
+    }
+}
