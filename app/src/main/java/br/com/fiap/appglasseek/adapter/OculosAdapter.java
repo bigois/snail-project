a495c06867e0b98f5054c18314d734cadada00e9
Guilherme Bigois <guilhermebigois@outlook.com>
2019-09-10 22:24:47 -0300

    Oculos Activity, Adapter and ViewHolder rewrited

--- app/src/main/java/br/com/fiap/appglasseek/adapter/OculosAdapter.java
+++ app/src/main/java/br/com/fiap/appglasseek/adapter/OculosAdapter.java
@@ -8,23 +8,24 @@ import android.view.ViewGroup;
 
 import java.text.DecimalFormat;
 import java.util.List;
+import java.util.Locale;
 
 import br.com.fiap.appglasseek.holder.OculosHolder;
 import br.com.fiap.appglasseek.model.Oculos;
 import br.com.fiap.appglasseek.R;
 
 public class OculosAdapter extends RecyclerView.Adapter<OculosHolder> {
-    private List<Oculos> oculosLista;
+    private List<Oculos> oculosList;
 
-    public OculosAdapter(List<Oculos> oculosLista){
-        this.oculosLista = oculosLista;
+    public OculosAdapter(List<Oculos> oculosList){
+        this.oculosList = oculosList;
     }
 
     @NonNull
     @Override
     public OculosHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
-        View view = layoutInflater.inflate(R.layout.items_compras, parent, false);
+        View view = layoutInflater.inflate(R.layout.holder_oculos_menu, parent, false);
         OculosHolder viewHolder = new OculosHolder(view);
 
         return viewHolder;
@@ -32,21 +33,20 @@ public class OculosAdapter extends RecyclerView.Adapter<OculosHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull OculosHolder holder, int position) {
-        if (oculosLista != null && oculosLista.size() > 0) {
-            Oculos oculos = oculosLista.get(position);
+        if (oculosList != null && oculosList.size() > 0) {
+            Oculos oculos = oculosList.get(position);
 
-            DecimalFormat decimalFormat = new DecimalFormat("#,##0.000000");
-            String preco = decimalFormat.format(oculos.getPreco());
+            Locale.setDefault(new Locale("pt", "BR"));
 
             holder.marca.setText(oculos.getMarca());
             holder.modelo.setText(oculos.getModelo());
-            holder.preco.setText(preco);
-
+            holder.preco.setText(new DecimalFormat("R$ #,##0.00").format(oculos.getPreco()));
+            holder.imagem.setImageResource(oculos.getImagem());
         }
     }
 
     @Override
     public int getItemCount() {
-        return oculosLista.size();
+        return oculosList.size();
     }
 }
--- app/src/main/java/br/com/fiap/appglasseek/fragment/InicioFragment.java
+++ app/src/main/java/br/com/fiap/appglasseek/fragment/InicioFragment.java
@@ -2,7 +2,6 @@ package br.com.fiap.appglasseek.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -10,46 +9,26 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import br.com.fiap.appglasseek.R;
-import br.com.fiap.appglasseek.activity.OculosActivity;
 import br.com.fiap.appglasseek.adapter.OculosAdapter;
-import br.com.fiap.appglasseek.model.Oculos;
+import br.com.fiap.appglasseek.dao.StaticData;
 
 public class InicioFragment extends Fragment {
-
-    private RecyclerView inicioRecycler;
-    private List<Oculos> oculosLista;
-    private RecyclerView.Adapter adapter;
-
-    public InicioFragment() {
-    }
+    public InicioFragment() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_inicio, container, false);
 
+        RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.rclInicio);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
-        View view = inflater.inflate(R.layout.fragment_inicio, container, false);
+        OculosAdapter oculosAdapter = new OculosAdapter(StaticData.OculosData.getOculosList());
+        oculosAdapter.notifyDataSetChanged();
 
-        inicioRecycler = (RecyclerView) view.findViewById(R.id.inicioRecycler);
+        recyclerView.setAdapter(oculosAdapter);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
 
-        inicioRecycler.setLayoutManager(new LinearLayoutManager(this.getActivity()));
-
-        oculosLista = new ArrayList<>();
-
-        Oculos oculosItem = new Oculos(
-                "1","Oakley","Juliet","Oculos de sol","M","Dourado",00.00, 50.00, 10.00,5.00,350.00,"Aço"
-
-        );
-        oculosLista.add(oculosItem);
-
-        adapter = new OculosAdapter(oculosLista);
-        inicioRecycler.setAdapter(adapter);
-
-        inicioRecycler.setItemAnimator(new DefaultItemAnimator());
-
-        return view;
+        return rootView;
     }
 }
