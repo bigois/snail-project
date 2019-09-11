f220a5c976408cf6a9430c85dec2031f73823839
GabrielMarquesC <gabrielmarquesc@yahoo.com>
2019-09-11 16:10:29 -0300

    Passing parameters to fragment_oculos

--- app/src/main/java/br/com/fiap/appglasseek/fragment/OculosFragment.java
+++ app/src/main/java/br/com/fiap/appglasseek/fragment/OculosFragment.java
@@ -7,12 +7,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
+
 import br.com.fiap.appglasseek.R;
 import br.com.fiap.appglasseek.model.Oculos;
 
 public class OculosFragment extends Fragment {
     private Oculos oculos;
-    private TextView txtCodigo;
+    private TextView txtCodigo, txtMarca, txtModelo, txtTipo, txtGenero, txtCor, txtComprimento, txtLargura, txtAltura, txtPreco, txtMaterial;
+
+
 
     public OculosFragment() {
     }
@@ -24,6 +28,39 @@ public class OculosFragment extends Fragment {
 
         txtCodigo = view.findViewById(R.id.txtCodigo);
         txtCodigo.setText(oculos.getCodigo());
+
+        txtMarca = view.findViewById(R.id.txtMarca);
+        txtMarca.setText(oculos.getMarca());
+
+        txtModelo = view.findViewById(R.id.txtModelo);
+        txtModelo.setText(oculos.getModelo());
+
+        txtTipo = view.findViewById(R.id.txtTipo);
+        txtTipo.setText(oculos.getTipo());
+
+        txtGenero = view.findViewById(R.id.txtGenero);
+        txtGenero.setText(oculos.getGenero());
+
+        txtCor = view.findViewById(R.id.txtCor);
+        txtCor.setText(oculos.getCor());
+
+        txtComprimento = view.findViewById(R.id.txtComprimento);
+        txtComprimento.setText(new DecimalFormat("#,##0.00").format(oculos.getComprimento()));
+
+        txtLargura = view.findViewById(R.id.txtLargura);
+        txtLargura.setText(new DecimalFormat("#,##0.00").format(oculos.getLargura()));
+
+        txtAltura = view.findViewById(R.id.txtAltura);
+        txtAltura.setText(new DecimalFormat("#,##0.00").format(oculos.getAltura()));
+
+        txtPreco = view.findViewById(R.id.txtPreco);
+        txtPreco.setText(new DecimalFormat("#,##0.00").format(oculos.getPreco()));
+
+        txtMaterial = view.findViewById(R.id.txtMaterial);
+        txtMaterial.setText(oculos.getMaterial());
+
+
+
 
         // TODO GABRIEL
         // RETORNAR A PARTIR DAQUI
