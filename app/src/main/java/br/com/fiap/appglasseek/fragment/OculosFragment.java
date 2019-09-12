229e51b6fb11f698f00319843e5b2f4ecfb217bc
Guilherme Bigois <guilhermebigois@outlook.com>
2019-09-11 22:18:18 -0300

    Added SliderView

--- app/src/main/java/br/com/fiap/appglasseek/fragment/OculosFragment.java
+++ app/src/main/java/br/com/fiap/appglasseek/fragment/OculosFragment.java
@@ -5,17 +5,19 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.ViewFlipper;
 
 import java.text.DecimalFormat;
 
 import br.com.fiap.appglasseek.R;
+import br.com.fiap.appglasseek.adapter.OculosAdapter;
 import br.com.fiap.appglasseek.model.Oculos;
 
 public class OculosFragment extends Fragment {
     private Oculos oculos;
     private TextView txtCodigo, txtMarca, txtModelo, txtTipo, txtGenero, txtCor, txtComprimento, txtLargura, txtAltura, txtPreco, txtMaterial;
-
 
 
     public OculosFragment() {
@@ -25,6 +27,8 @@ public class OculosFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_oculos, container, false);
+
+
 
         txtCodigo = view.findViewById(R.id.txtCodigo);
         txtCodigo.setText(oculos.getCodigo());
@@ -66,6 +70,8 @@ public class OculosFragment extends Fragment {
         // RETORNAR A PARTIR DAQUI
         // CRIAR LAYOUT E EXIBIR OS DADOS CONTIDOS NO OBJETO OCULOS
 
+
+
         return view;
     }
 
@@ -74,4 +80,5 @@ public class OculosFragment extends Fragment {
 
         return this;
     }
+
 }
